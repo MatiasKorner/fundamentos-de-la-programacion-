9e0ced5add6eb4d576949708fafdbac32ab39a4e
POAPCA02LC0127 <POAPCA01LC0400@POAPCA02LC0127.duoc.acad>
2025-06-16 14:18:44 -0400

    clase tuplas y diccionacio

---
+++ Diccionario.py
@@ -0,0 +1,11 @@
+diccionario = {
+    "rut": "20932332-k",
+    "nombre": "Matías Korner",
+    "direccion": "plaza oeste",
+    "fonos": [
+            993830581,
+            948968208,
+            997336333],
+    "activo": True}
+
+print(diccionario)
---
+++ tuplas.py
@@ -0,0 +1,6 @@
+# covertir una lista en tupla
+mi_lista = [1, 2, 3]
+mi_tupla = tuple(mi_lista)
+#imprimir la tupla
+print(mi_tupla)
+
